bb81abb352de00fbc845f70ef251ff10f6e19b6b
vmacena <macena.v@aluno.ifsp.edu.br>
2025-05-27 22:31:24 -0300

    feat: initialize project

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+        <ToastContainer
+          position="top-center"
+          toastClassName="custom-toast"
+        />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/pages/home/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { LoginForm } from '@/app/features/auth/LoginForm/index.';
+
+export default function HomePage() {
+  return <LoginForm />;
+}
